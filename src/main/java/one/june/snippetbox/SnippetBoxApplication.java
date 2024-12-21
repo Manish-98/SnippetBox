4e6160477fb083d5393aed94aa66ee0f94c4da0c
Manish Kumar Lath <manishlath3445@gmail.com>
2024-12-21 22:18:55 +0530

    Initial commit

---
+++ src/main/java/one/june/snippetbox/SnippetBoxApplication.java
@@ -0,0 +1,13 @@
+package one.june.snippetbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnippetBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnippetBoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/one/june/snippetbox/SnippetBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package one.june.snippetbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnippetBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
